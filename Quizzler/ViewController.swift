59e8ec25ba0bf17c6ab1f9438a17a566d994a710
MlleDolce <andreakchristie@gmail.com>
2019-01-04 19:38:06 -0500

    Created methods so that user can answer each quiz question until game is complete.  Game can detect end state and restarts upon finishing.  Labels and progress bar are not connected yet.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,31 +22,56 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        nextQuestion()
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        questionNumber += 1
+        
+        nextQuestion()
+        
     }
-    
     
     func updateUI() {
       
     }
     
-
     func nextQuestion() {
+        if questionNumber < allQuestions.list.count - 1 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        } else {
+            let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
         
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
     }
-    
     
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        correctAnswer == pickedAnswer ? print("You got it!") : print("Try again.")
+
     }
     
-    
     func startOver() {
-       
+        questionNumber = 0
+        nextQuestion()
     }
     
 
